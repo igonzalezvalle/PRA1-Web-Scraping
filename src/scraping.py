f3d290104e551d1da2b7f761cf5981c948ed3a68
cmerinogarc <62809468+cmerinogarc@users.noreply.github.com>
2020-03-29 18:34:47 +0200

    Update scraping.py

--- src/scraping.py
+++ src/scraping.py
@@ -1,1 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
 
+str = "https://supermercado.eroski.es/es/supermercado/SinGluten/"
+
+#Esto de abajo lo tendremos que meter en una función
+
+page = requests.get(str) #recuperamos la información correspondiente a la respuesta de la petición
+#page es un objeto. Sus atributos son:
+# - page.status_code: código HTTP devuelto por el servidor
+# - page.content: contenido en bruto de la respuesta del servidor
+
+soup = BeautifulSoup(page.content)
