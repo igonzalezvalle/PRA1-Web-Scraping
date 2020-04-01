c8e3fa4fe885780d8f61ac2151b87ab2ed838efc
igonzalezvalle <61950999+igonzalezvalle@users.noreply.github.com>
2020-04-01 22:59:11 +0200

    Update draft_prueba.py

--- src/draft_prueba.py
+++ src/draft_prueba.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from urllib.error import HTTPError
+import requests
 
 
 def getTitle(url):
@@ -22,10 +23,22 @@ else:
     print(title)
     
 html = urlopen("https://supermercado.eroski.es/es/login/delivery/?zipCode=48901")
-html2 = urlopen("https://supermercado.eroski.es/es/supermercado/SinGluten/")
-bsObj = BeautifulSoup(html2)
+urlpage = 'https://supermercado.eroski.es/es/supermercado/SinGluten/'
+page = requests.get(urlpage).text
+bsObj = BeautifulSoup(page, 'lxml')
 #print(bsObj)
 #Extraer el texto de la etiqueta
 nameList = bsObj.findAll("div", {"class":"product-description"})
+
+data = []
 for name in nameList:
-    print(name.get_text())
+    title = name.find("h2", {"class":"product-title product-title-resp"}).get_text()
+    rating = name.find("div", {"class":"ratingSubtitle"}).get_text()
+    price_before = name.find("span", {"class":"price-before"}).get_text() 
+    price_now = name.find("span", {"class":"price-now"}).get_text() 
+    #print(name.get_text())
+    #append dict to array
+    data.append({"articulo" : title, "valoracion" : rating, "precio_antes": price_before, "precio_actual" : price_now})
+    print(title, rating, price_before, price_now)
+    
+print (data)
