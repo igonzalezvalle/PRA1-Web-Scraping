f01df059c190f2422516a2de9a4bae77762e48a7
igonzalezvalle <61950999+igonzalezvalle@users.noreply.github.com>
2020-04-03 17:49:51 +0200

    Update scraping.py

--- src/scraping.py
+++ src/scraping.py
@@ -47,24 +47,36 @@ soup = BeautifulSoup(page, "html.parser")
 #Extraer el texto de la etiqueta
 nameList = soup.findAll("div", {"class":"product-description"})
 
+#Inicializamos el diccionario de datos donde cargaremos la información
 data = []
-for name in nameList:
+
+for i, name in enumerate(nameList):
     title = name.find("h2", {"class":"product-title product-title-resp"}).text
+    title = title.strip() #limpiamos la variable, dejando solo el texto
     product_name = title.split(',')[0] #Nos devuelve el nombre sin el peso (Filetes de lomo adobado de cerdo extrafino EROSKI)
+    product_name = product_name.strip()
     product_quantity = title.split(' ',1)[1].split(',')[1].strip() #Nos devuelve la cantidad (bandeja 300 g)
+    product_quantity = product_quantity.strip()
     #Sacar el valor nutricional
+    
     #quantity_product = name.find("span", {"class":"quantity-product"}).text
     #price_product = name.find("span", {"class":"price-product"}).text
     #Rating - número valoraciones usuarios
     rating = name.find("div", {"class":"ratingSubtitle"}).get_text()
+    rating = rating.strip()
     price_before = name.find("span", {"class":"price-before"}).get_text() 
+    price_before = price_before.strip()
     price_now = name.find("span", {"class":"price-now"}).get_text() 
+    price_now = price_now.strip()
     #print(name.get_text())
     #append dict to array
+       
+    #Quitar retornos de carro (\n) y dejar solo precio y €. En la valoración dejar solo la puntuación
     data.append({"articulo" : title, "Nombre" : product_name, "Camtidad" : product_quantity, "valoracion" : rating, 
                  "precio_antes": price_before, "precio_actual" : price_now})
-    print(title, rating, price_before, price_now)
+    print("%d|%s | %s|%s|%s|%s|%s| " %(i+1,title, product_name, product_quantity,  rating, price_before, price_now))
     
+#Mostramos el diccionario con toda la información cargada.    
 print (data)    
 
 #ProductInfo = soup.find("h2", {"class": "product-title product-title-resp"}).text
