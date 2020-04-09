14393df13aafc6d5d36ef5cc532a2a5b53e3ea59
igonzalezvalle <61950999+igonzalezvalle@users.noreply.github.com>
2020-04-09 18:04:15 +0200

    Update scraping.py

--- src/scraping.py
+++ src/scraping.py
@@ -28,65 +28,64 @@ def getTitle(url):
 #Identificamos la url que vamos a rastrear
 str = "https://supermercado.eroski.es/es/supermercado/SinGluten/"
 
-
+# Comprobamos el acceso y sacamos el itulo de la página por pantalla
 title = getTitle(str)
 if title == None:
     print("Title could not be found")
 else:
     print(title)
 #podríamos meter en el else toda la lógica
-    
-#html = urlopen("https://supermercado.eroski.es/es/login/delivery/?zipCode=48901")
-#urlpage = 'https://supermercado.eroski.es/es/supermercado/SinGluten/'
 
 
 #Creamos la función de scroll infinito (cuando llamemos a esta función recorrerá todas las páginas disponibles al hacer el scroll hacia abajo
-#def scroll(driver, timeout):
-
-#    last_height = driver.execute_script("return document.body.scrollHeight")
-#    # el tiempo de espera vendrá definido como uno de los valores de la función
-#    Scroll_Wait = timeout
-
-#    i=0 # contador de scroll
-#    while True:
-#        # execute script to scroll down the page
-#        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        
-#        time.sleep(Scroll_Wait)
-#        # Calculate new scroll height and compare with last scroll height
-#        new_height = driver.execute_script("return document.body.scrollHeight")
-#        i=i+1
-#        print ("scroll número", i)
-#        if new_height == last_height:
-#            break
-#        last_height = new_height
-#    print ("fin del scroll")
-
-
 def scroll(driver, timeout):
 
-    for i in range (1,40):
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    # el tiempo de espera vendrá definido como uno de los valores de la función
+    Scroll_Wait = timeout
+
+    i=0 # contador de scroll
+    while True:
         # execute script to scroll down the page
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        
+        time.sleep(Scroll_Wait)
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        i=i+1
+        print ("scroll número", i)
+        if new_height == last_height:
+            break
+        last_height = new_height
+    print ("fin del scroll")
+
+
+#def scroll(driver, timeout):
+
+#    for i in range (1,40):
+#        # execute script to scroll down the page
+#        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
         # sleep
-        time.sleep(timeout)
+#        time.sleep(timeout)
 
 
 #Identificamos el driver    
 #driver = webdriver.Chrome("C:/chromedriver.exe") #la ruta donde tengamos el ejecutable
-
+#Vamos a utilizar PhnatomJS que nos permite realizar las mismas acciones que con el driver de chrome, 
+# pero abriéndolo en segundo plano.
+#Para su ejecución hay que tener en cuenta el directorio donde hemos descargado el ejecutable.
 driver = webdriver.PhantomJS("C:/phantomjs.exe")
 
 options = Options()
 options.headless = True #No abrimos el navegador
 #driver = webdriver.Firefox(firefox_options=options, executable_path = "C:/geckodriver.exe")
 
-#Creamos una espera previa a dar un error
+#Creamos una espera previa en el caso de que nos devuelva algún error dar un error
 driver.implicitly_wait(100)
 #abrimos la página
 driver.get(str)
 
-#Llamamos a la función de scroll
+#Llamamos a la función de scroll con una espera entre scroll hacia abajo de 7 sg
 scroll (driver, 5)
 
 #Parseamos el html resultante con toda la información
@@ -103,6 +102,16 @@ nameList = soup.findAll("div", {"class":"product-description"})
 data = []
 #recorremos todas las etiquetas para crear el dataset
 for i, name in enumerate(nameList):
+    #Sacar el valor nutricional. Hay que buscarlo en el texto
+    #<a class="nutriscore score-c" que está un nivel por encima de donde estamos ahora
+    nutrition = name.find("div", {"class":"description-text"}).parent.parent
+    if nutrition.find("a", {"class":"nutriscore"})==None:
+        nutrition_score = "NaN"
+    else:
+        nutrition = nutrition.find("a", {"class":"nutriscore"})
+        nutrition_score = nutrition.get("class")[1]
+   
+    #Sacamos la información del producto (nombre y presentación)
     title = name.find("h2", {"class":"product-title product-title-resp"}).text
     title = title.strip()
     product_name = title.split(',')[0] #Nos devuelve el nombre sin el peso (Filetes de lomo adobado de cerdo extrafino EROSKI)
@@ -110,22 +119,20 @@ for i, name in enumerate(nameList):
     # product quantity es presentación del producto
     product_quantity = title.split(' ',1)[1].split(',')[1].strip() #Nos devuelve la cantidad (bandeja 300 g)
     product_quantity = product_quantity.strip()
-    #Sacar el valor nutricional. Hay que buscarlo en el texto
-    #<a class="nutriscore score-c"
-    nutrition = name.find("a", text="nutriscore score-")
+
     # Las siguientes etiquetas no se encuentran informadas en todos los casos, lo que hacemos es, 
     #en el caso de que no existan poner valos NaN y en otro caso recuperar la información
     if name.find("span", {"class":"quantity-product"})==None:
         quantity_product = "NaN"
     else:
         quantity_product = name.find("span", {"class":"quantity-product"}).text
-        quantity_product = quantity_product.strip()
+        quantity_product = quantity_product.split(' ')[1].strip()
         
     if name.find("span", {"class":"price-product"}) == None:
         price_product = "NaN"
     else:
         price_product = name.find("span", {"class":"price-product"}).text
-        price_product = price_product.strip()
+        price_product = price_product.split(' ')[0].strip()
         
     #Rating - número valoraciones usuarios
     rating = name.find("div", {"class":"ratingSubtitle"}).get_text()
@@ -143,34 +150,14 @@ for i, name in enumerate(nameList):
     price_now = name.find("span", {"class":"price-offer-now"}).get_text() 
     price_now = price_now.strip()
     #print(name.get_text())
+
     #append dict to array
-    
-    #Quitar retornos de carro (\n) y dejar solo precio y €. En la valoración dejar solo la puntuación
-    data.append({"articulo" : title, "Nombre" : product_name, "Presentación" : product_quantity, "valor nutricional" : nutrition, 
+    data.append({"id": i, "articulo" : title, "Nombre" : product_name, "Presentación" : product_quantity, "valor nutricional" : nutrition_score, 
                  "cantidad base" : quantity_product, "precio por cantidad base" : price_product,"valoracion" : rating, 
                  "precio_antes": price_before, "precio_actual" : price_now, "fecha": date.today()})
-    print("%d|%s | %s|%s|%s|%s|%s|%s|%s|%s " %(i+1,title, product_name, product_quantity, nutrition, quantity_product, 
-                                               price_product, rating, price_before, price_now))
     
 print (data)    
 
-#ProductInfo = soup.find("h2", {"class": "product-title product-title-resp"}).text
-#print(ProductInfo) 
-
-#<a href="/es/productdetail/14085807-filetes-de-lomo-adobado-de-cerdo-extrafino-eroski-bandeja-300-g/">Filetes de lomo adobado de cerdo extrafino EROSKI, bandeja 300 g</a>
-
-#<h2 class="product-title product-title-resp">
-#				<a href="/es/productdetail/20110177-carne-picada-de-ternera-eusko-label-seleqtia-bandeja-500-g/">
-#					Filetes de lomo adobado de cerdo extrafino EROSKI, bandeja 300 g
-#				</a>
-#			</h2>
-
-#Filetes de lomo adobado de cerdo extrafino EROSKI, bandeja 300 g
-#
-#
-
-#ProductName = ProductInfo.split(',')[0]  #Nos devuelve el nombre sin el peso (Filetes de lomo adobado de cerdo extrafino EROSKI)
-#ProductQuantity = ProductInfo.split(' ',1)[1].split(',')[1].strip() #Nos devuelve la cantidad (bandeja 300 g)
 
 #Como último paso trasladamos los datos a un dataframe para poder volcarlos a un csv
 df=pd.DataFrame(data)
